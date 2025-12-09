bd97995d0e0d58d443071f3ecadad5645259be76
Kedia12 <kediaihogoza01@gmail.com>
2025-12-09 13:31:27 +0000

    fix: removed trailing whitespace

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,9 +4,8 @@
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to the node to check
  *
- * Return: 1 if node is a leaf, otherwise 0, 
+ * Return: 1 if node is a leaf, otherwise 0,
  * 0 if node is NULL
- *
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
